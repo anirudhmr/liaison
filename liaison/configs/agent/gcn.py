27752f8afc4699939f4008b88b5eafb3143c3665
arc <addanki@mit.edu>
2019-11-04 22:20:36 +0000

    Add gcn config

---
+++ liaison/configs/agent/gcn.py
@@ -0,0 +1,41 @@
+from liaison.utils import ConfigDict
+"""
+Must have attribute class_path attribute.
+"""
+
+
+def get_config():
+  config = ConfigDict()
+
+  # required fields.
+  config.class_path = "liaison.agents.gcn"
+  config.class_name = "Agent"
+
+  config.model = ConfigDict()
+  config.model.class_path = "liaison.agents.models.gcn"
+  config.model.n_prop_layers = 8
+  config.model.node_embed_dim = 32
+
+  config.lr_init = 1e-3
+  config.lr_min = 1e-4
+  config.lr_start_dec_step = 1000
+  config.lr_dec_steps = 5000
+  config.lr_dec_val = .1
+  config.lr_dec_approach = 'linear'
+
+  config.ent_dec_init = 1
+  config.ent_dec_min = 0
+  config.ent_dec_steps = 1000
+  config.ent_start_dec_step = 1000
+  config.ent_dec_val = .1
+  config.ent_dec_approach = 'linear'
+
+  config.grad_clip = 1.0
+  config.discount_factor = 0.99
+  config.clip_rho_threshold = 1.0
+  config.clip_pg_rho_threshold = 1.0
+
+  config.loss = ConfigDict()
+  config.loss.vf_loss_coeff = 1.0
+
+  return config
